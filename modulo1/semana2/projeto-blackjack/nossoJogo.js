7335a38555ea38af64c1b2401f01b511796bcf74
MariTronix <filhatronixmmlima@gmail.com>
2022-12-15 12:21:24 -0300

    Inico do desafio

--- modulo1/semana2/projeto-blackjack/nossoJogo.js
+++ modulo1/semana2/projeto-blackjack/nossoJogo.js
@@ -1,35 +1,104 @@
-/**
- * EXEMPLO DE UTILIZAÇÃO DA 'comprarCarta'
- * 
- * 
-    const carta = comprarCarta(); // Sorteia uma carta. Por exemplo, o rei de ouros
-    
-    console.log(carta.texto) // imprime o texto da carta. Exemplo: "K♦️" (indica "K" de ouros)
-    console.log(carta.valor) // imprime o valor da carta (um número). Exemplo: 10 (dado que "K" vale 10)
- * 
- * 
- * 
- */
-
 console.log("Boas vindas ao jogo de Blackjack!");
 
-if (confirm("Quer iniciar uma nova rodada?")){
+
+if (confirm
+   (`Boas vindas ao jogo de Blackjack! 
+Quer iniciar uma nova rodada?"`
+   )){
    const cartaDoUsuario = comprarCarta();
    const cartaDoUsuario1 = comprarCarta();
    const cartaDoComputador = comprarCarta();
    const cartaDoComputador1 = comprarCarta();
    const somaDoUsuario = (cartaDoUsuario.valor + cartaDoUsuario1.valor);
    const somaDoComputador = (cartaDoComputador.valor + cartaDoComputador1.valor);
-   console.log(`Cartas do usuario ${cartaDoUsuario.texto}, ${cartaDoUsuario1.texto} Pontuação: ${somaDoUsuario}`);
-   console.log(`Cartas do computador ${cartaDoComputador.texto}, ${cartaDoComputador1.texto} pontuação: ${somaDoComputador}`);
-if (somaDoComputador === somaDoUsuario){
-console.log("Jogo Empatado")
-} else if (somaDoComputador > somaDoUsuario){
-   console.log("Computador venceu")
-} else if (somaDoComputador < somaDoUsuario){
-   console.log("Você venceu")
+   
+if(somaDoUsuario < 21 && (somaDoComputador < 21 || somaDoComputador !== 21)){
+
+   console.log(`Usuário - cartas: ${cartaDoUsuario.texto} ${cartaDoUsuario1.texto} - ${somaDoUsuario}`)
+   console.log(`Computador - cartas: ${cartaDoComputador.texto} ${cartaDoComputador1.texto} - ${somaDoComputador}`)
+
+   if(confirm(`Suas cartas são ${cartaDoUsuario.texto} ${cartaDoUsuario1.texto}. A soma da suas cartas são ${somaDoUsuario}. 
+A carta revelada do computador é ${cartaDoComputador.texto}. 
+Deseja comprar mais uma carta?`)) {
+   const cartaDoUsuarioExtra = comprarCarta();
+   const segundaSomaDoUsuario = (somaDoUsuario + cartaDoUsuarioExtra.valor);
+      if(segundaSomaDoUsuario < 21){
+         if(confirm(`Suas cartas são ${cartaDoUsuario.texto} ${cartaDoUsuario1.texto}.` + "/n" + `A soma da suas cartas são ${somaDoUsuario}.` +  `A carta revelada do computador é ${cartaDoComputador.texto}.` + "/n" +  `Deseja comprar mais uma carta?`)) {
+            
+      }
+   } else{
+      if (somaDoComputador === somaDoUsuario){
+    console.log("Jogo Empatado")
+    } else if (somaDoComputador > somaDoUsuario){
+       console.log("Computador venceu")
+    } else if (somaDoComputador < somaDoUsuario){
+       console.log("Você venceu")
+    }else {
+       console.log("Jogo Finalizado")
+    } 
+
+   }
+
+
+
+} else{
+   if (somaDoComputador === somaDoUsuario){
+ console.log("Jogo Empatado")
+ } else if (somaDoComputador > somaDoUsuario){
+    console.log("Computador venceu")
+ } else if (somaDoComputador < somaDoUsuario){
+    console.log("Você venceu")
+ }else {
+    console.log("Jogo Finalizado")
+ } 
 }
-} else {
-   console.log("Jogo Finalizado")
+} else{
+   if (somaDoComputador === somaDoUsuario){
+ console.log("Jogo Empatado")
+ } else if (somaDoComputador > somaDoUsuario){
+    console.log("Computador venceu")
+ } else if (somaDoComputador < somaDoUsuario){
+    console.log("Você venceu")
+ }else {
+    console.log("Jogo Finalizado")
+ } 
 }
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+{
+   let carta1UsuarioExtra = comprarCarta()
+   const somaDoUsuarioTotal = somaDoUsuario.valor + carta1UsuarioExtra.valor  
+   if(confirm(`Usuário - Carta:${cartaDoUsuario.texto} ${cartaDoUsuario1.texto} ${cartaDoUsuarioExtra.texto} - Pontuaçã: ${somaDoUsuarioTotal}
+Computador - Carta` )){
+
+   }
+}
+   // confirm(`Usuario - Cartas ${cartaDoUsuario} ${cartaDoUsuario1} - Pontuação: 4{}`)
+   // console.log(`Cartas do usuario ${cartaDoUsuario.texto}, ${cartaDoUsuario1.texto} Pontuação: ${somaDoUsuario}`);
+   console.log(`Cartas do computador ${cartaDoComputador.texto}, ${cartaDoComputador1.texto} pontuação: ${somaDoComputador}`);
+}
