681eed01086c522952f7ce1f401a5620554ef161
MariTronix <filhatronixmmlima@gmail.com>
2022-12-16 21:58:26 -0300

    Comprar cartas: Feito - Não inciar com carta A: Não feito

--- modulo1/semana2/projeto-blackjack/nossoJogo.js
+++ modulo1/semana2/projeto-blackjack/nossoJogo.js
@@ -5,100 +5,128 @@ if (confirm
    (`Boas vindas ao jogo de Blackjack! 
 Quer iniciar uma nova rodada?"`
    )){
-   const cartaDoUsuario = comprarCarta();
-   const cartaDoUsuario1 = comprarCarta();
-   const cartaDoComputador = comprarCarta();
-   const cartaDoComputador1 = comprarCarta();
-   const somaDoUsuario = (cartaDoUsuario.valor + cartaDoUsuario1.valor);
-   const somaDoComputador = (cartaDoComputador.valor + cartaDoComputador1.valor);
-   
-if(somaDoUsuario < 21 && (somaDoComputador < 21 || somaDoComputador !== 21)){
+   let cartaDoUsuario = comprarCarta();
+   let cartaDoUsuario1 = comprarCarta();
+   let cartaDoComputador = comprarCarta();
+   let cartaDoComputador1 = comprarCarta();
+   let somaDoUsuario = (cartaDoUsuario.valor + cartaDoUsuario1.valor);
+   let somaDoComputador = (cartaDoComputador.valor + cartaDoComputador1.valor);
 
-   console.log(`Usuário - cartas: ${cartaDoUsuario.texto} ${cartaDoUsuario1.texto} - ${somaDoUsuario}`)
-   console.log(`Computador - cartas: ${cartaDoComputador.texto} ${cartaDoComputador1.texto} - ${somaDoComputador}`)
+   if(cartaDoUsuario === "A"){
+      cartaDoUsuario = comprarCarta();
+      somaDoComputador = (cartaDoUsuario.valor + cartaDoUsuario1.valor);
 
-   if(confirm(`Suas cartas são ${cartaDoUsuario.texto} ${cartaDoUsuario1.texto}. A soma da suas cartas são ${somaDoUsuario}. 
-A carta revelada do computador é ${cartaDoComputador.texto}. 
-Deseja comprar mais uma carta?`)) {
-   const cartaDoUsuarioExtra = comprarCarta();
-   const segundaSomaDoUsuario = (somaDoUsuario + cartaDoUsuarioExtra.valor);
-      if(segundaSomaDoUsuario < 21){
-         if(confirm(`Suas cartas são ${cartaDoUsuario.texto} ${cartaDoUsuario1.texto}.` + "/n" + `A soma da suas cartas são ${somaDoUsuario}.` +  `A carta revelada do computador é ${cartaDoComputador.texto}.` + "/n" +  `Deseja comprar mais uma carta?`)) {
-            
+      if(somaDoUsuario < 21 && (somaDoComputador < 21 || somaDoComputador !== 21)){
+      console.log(`Usuário - cartas: ${cartaDoUsuario.texto} ${cartaDoUsuario1.texto} - ${somaDoUsuario}`)
+      console.log(`Computador - cartas: ${cartaDoComputador.texto} ${cartaDoComputador1.texto} - ${somaDoComputador}`)
+      if(confirm(
+         `Suas cartas são ${cartaDoUsuario.texto} ${cartaDoUsuario1.texto}. A soma da suas cartas são ${somaDoUsuario}. 
+         A carta revelada do computador é ${cartaDoComputador.texto}. 
+         Deseja comprar mais uma carta?`)){
+         const cartaDoUsuarioExtra = comprarCarta();
+         const segundaSomaDoUsuario = (somaDoUsuario + cartaDoUsuarioExtra.valor);
+         console.log(`Usuário - cartas: ${cartaDoUsuario.texto} ${cartaDoUsuario1.texto} ${cartaDoUsuarioExtra.texto} - ${segundaSomaDoUsuario}`)
+         console.log(`Computador - cartas: ${cartaDoComputador.texto} ${cartaDoComputador1.texto} - ${somaDoComputador}`)
+
+         if(segundaSomaDoUsuario < 21){
+            if(confirm(`Suas cartas são ${cartaDoUsuario.texto} ${cartaDoUsuario1.texto} ${cartaDoUsuarioExtra.texto}. A soma da suas cartas são ${segundaSomaDoUsuario}.  
+            A carta revelada do computador é ${cartaDoComputador.texto}.
+            Deseja comprar mais uma carta?`)) {
+               const cartaDoUsuarioExtra1 = comprarCarta();
+               const terceiraSomaDoUsuario = (segundaSomaDoUsuario + cartaDoUsuarioExtra1.valor);
+               if (somaDoComputador === terceiraSomaDoUsuario){
+                  console.log("Jogo Empatado");
+                  alert("Jogo Empatado");
+               } else if (somaDoComputador > terceiraSomaDoUsuario){
+                  console.log("Computador venceu");
+                  alert("Computador venceu");
+               } else if (terceiraSomaDoUsuario > 21){
+                  console.log("Computador venceu");
+                  alert("Computador vencendo");
+               } else if (somaDoComputador < terceiraSomaDoUsuario){
+                  console.log("Você venceu");
+                  alert("Você venceu");
+               }else {
+                  console.log("Jogo Finalizado");
+                  alert("Jogo Finalizado");
+               } 
+            } else{
+               if (somaDoComputador === segundaSomaDoUsuario){
+                  console.log("Jogo Empatado");
+                  alert("Jogo Empatado");
+               } else if (somaDoComputador > segundaSomaDoUsuario){
+                  console.log("Computador venceu");
+                  alert("Computador venceu");
+               } else if (segundaSomaDoUsuario > 21){
+                  console.log("Computador venceu");
+                  alert("Computador vencendo");
+               } else if (somaDoComputador < segundaSomaDoUsuario){
+                  console.log("Você venceu");
+                  alert("Você venceu");
+               }else {
+                  console.log("Jogo Finalizado");
+                  alert("Jogo Finalizado");
+               } 
+               }
+            } else{
+               if (somaDoComputador === segundaSomaDoUsuario){
+                  console.log("Jogo Empatado");
+                  alert("Jogo Empatado");
+               } else if (somaDoComputador > segundaSomaDoUsuario){
+                  console.log("Computador venceu");
+                  alert("Computador venceu");
+               } else if (segundaSomaDoUsuario > 21){
+                  console.log("Computador venceu");
+                  alert("Computador vencendo");
+               } else if (somaDoComputador < segundaSomaDoUsuario){
+                  console.log("Você venceu");
+                  alert("Você venceu");
+               }else {
+                  console.log("Jogo Finalizado");
+                  alert("Jogo Finalizado");
+               } 
+            }  
+      } else{
+         if (somaDoComputador === somaDoUsuario){
+            console.log("Jogo Empatado");
+            alert("Jogo Empatado");
+            } else if (somaDoComputador > somaDoUsuario){
+               console.log("Computador venceu");
+               alert("Computador vencendo");
+            } else if (somaDoUsuario > 21){
+               console.log("Computador venceu");
+               alert("Computador vencendo");
+            } else if (somaDoComputador < somaDoUsuario){
+               console.log("Você venceu");
+               alert("Você venceu");
+            }else {
+               console.log("Jogo Finalizado");
+               alert("Jogo Finalizado");
+            } 
       }
-   } else{
+      } else{
       if (somaDoComputador === somaDoUsuario){
-    console.log("Jogo Empatado")
-    } else if (somaDoComputador > somaDoUsuario){
-       console.log("Computador venceu")
-    } else if (somaDoComputador < somaDoUsuario){
-       console.log("Você venceu")
-    }else {
-       console.log("Jogo Finalizado")
-    } 
+         console.log("Jogo Empatado");
+         alert("Jogo Empatado");
+         } else if (somaDoComputador > somaDoUsuario){
+            console.log("Computador venceu");
+            alert("Computador vencendo");
+         } else if (somaDoUsuario > 21){
+            console.log("Computador venceu");
+            alert("Computador vencendo");
+         } else if (somaDoComputador < somaDoUsuario){
+            console.log("Você venceu");
+            alert("Você venceu");
+         }else {
+            console.log("Jogo Finalizado");
+            alert("Jogo Finalizado");
+         } 
+      }
 
-   }
+   }else{}
 
-
-
-} else{
-   if (somaDoComputador === somaDoUsuario){
- console.log("Jogo Empatado")
- } else if (somaDoComputador > somaDoUsuario){
-    console.log("Computador venceu")
- } else if (somaDoComputador < somaDoUsuario){
-    console.log("Você venceu")
- }else {
-    console.log("Jogo Finalizado")
- } 
-}
-} else{
-   if (somaDoComputador === somaDoUsuario){
- console.log("Jogo Empatado")
- } else if (somaDoComputador > somaDoUsuario){
-    console.log("Computador venceu")
- } else if (somaDoComputador < somaDoUsuario){
-    console.log("Você venceu")
- }else {
-    console.log("Jogo Finalizado")
- } 
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-{
-   let carta1UsuarioExtra = comprarCarta()
-   const somaDoUsuarioTotal = somaDoUsuario.valor + carta1UsuarioExtra.valor  
-   if(confirm(`Usuário - Carta:${cartaDoUsuario.texto} ${cartaDoUsuario1.texto} ${cartaDoUsuarioExtra.texto} - Pontuaçã: ${somaDoUsuarioTotal}
-Computador - Carta` )){
-
-   }
-}
-   // confirm(`Usuario - Cartas ${cartaDoUsuario} ${cartaDoUsuario1} - Pontuação: 4{}`)
-   // console.log(`Cartas do usuario ${cartaDoUsuario.texto}, ${cartaDoUsuario1.texto} Pontuação: ${somaDoUsuario}`);
-   console.log(`Cartas do computador ${cartaDoComputador.texto}, ${cartaDoComputador1.texto} pontuação: ${somaDoComputador}`);
-}
+   
+} else {
+         console.log("Jogo Finalizado")
+         alert("Jogo Finalizado")
+      }
